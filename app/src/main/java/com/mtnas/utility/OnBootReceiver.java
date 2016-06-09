11f719878107ba07f720a0981a4634fb5c9e64f7
Moses Mwongela <moses1889@gmail.com>
2016-06-09 13:08:55 +0000

    OnBootReceiver

---
+++ app/src/main/java/com/mtnas/utility/OnBootReceiver.java
@@ -0,0 +1,20 @@
+package com.mtnas.utility;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ComponentInfo;
+
+/**
+ * Created by mwongela on 6/9/16.
+ */
+public class OnBootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = ComponentInfo.class.getCanonicalName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
+
